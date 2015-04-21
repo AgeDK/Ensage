4504ca00791611edbc4e1b6d35d5edf6ea6fe105
AgeDK <Alex.Samsurs@gmail.com>
2015-04-21 16:36:44 +0300

    Update Chicken.lua

--- Scripts/Chicken.lua
+++ Scripts/Chicken.lua
@@ -5,20 +5,17 @@ require("libs.Utils")
 
 local config = ScriptConfig.new()
 config:SetParameter("Hotkey", "Y", config.TYPE_HOTKEY)
+config:SetParameter("distance", 1200)
 config:SetParameter("Xcord", 500)
 config:SetParameter("Ycord", 50)
 config:Load()
 
-toggleKey = config.Hotkey
-x = config.Xcord
-y = config.Ycord
-
-local play = false local activated = false local giveitem = false
+local play = false local activated = false local giveitem = false local safety = false local delay = 0
 local font = drawMgr:CreateFont("chicken","Tahoma",14,500)
-local text = drawMgr:CreateText(x,y,0xFFFF00FF,"Hello, I'm Waiting for lick your bottle!",font) text.visible = false
+local text = drawMgr:CreateText(config.Xcord,config.Ycord,0xFFFF00FF,"Hello, I'm Waiting for lick your bottle!",font) text.visible = false
 
 function Key(msg,code)
-	if msg ~= KEY_UP and code == toggleKey and not client.chat then
+	if msg ~= KEY_UP and code == config.Hotkey and not client.chat then
 		if not activated then
 			activated = true
 			text.text = "Hello, I'm Waiting for lick your bottle!"
@@ -32,31 +29,69 @@ end
 function Tick(tick)
 	if not PlayingGame() then return end
 	local me = entityList:GetMyHero() local mp = entityList:GetMyPlayer()
-	if activated and SleepCheck("chicken") then
-	local chicken = entityList:FindEntities({classId = CDOTA_Unit_Courier,team = me.team,alive = true})[1] if not chicken then return end
-		if chicken and chicken:GetAbility(6):CanBeCasted() then
-			chicken:CastAbility(chicken:GetAbility(6))
-			Sleep(250+client.latency, "chicken")
+	local chicken = entityList:FindEntities({classId=CDOTA_Unit_Courier,team=me.team,alive=true})[1]
+	if chicken then
+		if tick > delay and SleepCheck("Zzz") then
+			local bottle = me:FindItem("item_bottle")
+			local heros = entityList:GetEntities({type=LuaEntity.TYPE_HERO,visible=true,alive=true,team=me:GetEnemyTeam()})
+			for i,v in ipairs(heros) do
+				if GetDistance2D(chicken,v) <= config.distance and chicken:GetAbility(1):CanBeCasted() then
+					chicken:CastAbility(chicken:GetAbility(1))
+					Boost(chicken)
+					Sleep(client.latency,"Zzz")
+					safety = false
+				else
+					safety = true
+				end
+			end
+			if activated and safety then
+				if bottle and bottle.charges == 0 then
+					giveitem = true
+					CheckStash(chicken)
+					chicken:Follow(me)
+					Sleep(client.latency,"Zzz")
+				end
+				if GetDistance2D(chicken,me) <= 250 and bottle and bottle.charges == 0  then
+					giveitem = false
+					Deliver(chicken)
+					mp:GiveItem(chicken,bottle)
+					Sleep(client.latency,"Zzz")
+				end
+				local chickenbottle = chicken:FindItem("item_bottle")
+				if chickenbottle and chickenbottle.charges == 0 and chicken:GetAbility(1):CanBeCasted() then
+					chicken:CastAbility(chicken:GetAbility(1))
+					Boost(chicken)
+					Sleep(client.latency,"Zzz")
+				end
+				if chickenbottle and chickenbottle.charges == 3 and chicken:GetAbility(5):CanBeCasted() then
+					chicken:CastAbility(chicken:GetAbility(5))
+					CheckStash(chicken)
+					Boost(chicken)
+					Sleep(client.latency,"Zzz")
+				end
+			end
+			delay = tick + 550
 		end
-		local bottle = me:FindItem("item_bottle") 
-		if bottle and bottle.charges == 0 then
-			giveitem = true
-			chicken:Follow(me)
-			Sleep(250+client.latency, "chicken")
-		end
-		if GetDistance2D(chicken,me) <= 200 and bottle and bottle.charges == 0  then
-			giveitem = false
-			mp:GiveItem(chicken,bottle)
-			Sleep(250+client.latency, "chicken")
-		end
-		local chickenbottle = chicken:FindItem("item_bottle")
-		if chickenbottle and chickenbottle.charges == 0 and chicken:GetAbility(1):CanBeCasted() then
-			chicken:CastAbility(chicken:GetAbility(1))
-			Sleep(250+client.latency, "chicken")
-		end
-		if chickenbottle and chickenbottle.charges == 3 and chicken:GetAbility(5):CanBeCasted() then
-			chicken:CastAbility(chicken:GetAbility(5))
-			Sleep(250+client.latency, "chicken")
+	end
+end
+
+function Deliver(chicken)
+	if chicken and chicken:GetAbility(5):CanBeCasted() then
+		chicken:CastAbility(chicken:GetAbility(5))
+	end
+end
+
+function Boost(chicken)
+	if chicken and chicken:GetAbility(6):CanBeCasted() then
+		chicken:CastAbility(chicken:GetAbility(6))
+	end
+end
+
+function CheckStash(chicken)
+	for i = 7, 12 do
+		local item = entityList:GetMyHero():GetItem(i)
+		if item and chicken:GetAbility(4):CanBeCasted() then
+			chicken:CastAbility(chicken:GetAbility(4))
 		end
 	end
 end
@@ -64,6 +99,7 @@ end
 function Load()
 	if PlayingGame() then
 		play = true
+		safety = true
 		activated = true
 		text.visible = true
 		script:RegisterEvent(EVENT_KEY,Key)
@@ -75,6 +111,7 @@ end
 function Close()
 	activated = false
 	giveitem = false
+	safety = false
 	text.visible = false
 	collectgarbage("collect")
 	if play then
