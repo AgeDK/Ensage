0ef4febe75e79e6973d2482de99f07792a87fa81
AgeDK <Alex.Samsurs@gmail.com>
2015-07-27 18:01:12 +0300

    Update Disabler.lua

--- Scripts/Disabler.lua
+++ Scripts/Disabler.lua
@@ -1,27 +1,78 @@
---<<Disabler of blinkers and initiators>>
---===By Blaxpirit===--
-
 require("libs.Utils")
 require("libs.ScriptConfig")
-require("libs.Initiators")
-require("libs.DisableSpells")
 
-local config = ScriptConfig.new()
+config = ScriptConfig.new()
 config:SetParameter("Active", "113", config.TYPE_HOTKEY)
 config:SetParameter("indent", 255)
 config:Load()
 
-local active = false local play = false local activated = 0 local hero = {} local icon = {} local sleepTick = nil
+monitor = client.screenSize.x/1600
+statusText = drawMgr:CreateText(290*monitor,42*monitor,0xFFFF00FF,"Disabler: Blink",drawMgr:CreateFont("text","Arial",11*monitor,550*monitor)) statusText.visible = false
 
-local monitor = client.screenSize.x/1600
-local F11 = drawMgr:CreateFont("F11","Tahoma",11*monitor,550*monitor) 
-local statusText = drawMgr:CreateText(290*monitor,42*monitor,0xFF3399FF,"Disabler: Blink",F11) statusText.visible = false
- 
+active, play, activated, hero, icon, sleepTick = false, false, 0, {}, {}, nil
+
+DisableSpell = {
+	npc_dota_hero_crystal_maiden = {Spell = "crystal_maiden_frostbite"},
+	npc_dota_hero_drow_ranger = {Spell = "drow_ranger_silence"},
+	npc_dota_hero_morphling = {Spell = "morphling_adaptive_strike"},
+	npc_dota_hero_sandking = {Spell = "sandking_burrowstrike"},
+	npc_dota_hero_storm_spirit = {Spell = "storm_spirit_electric_vortex"},
+	npc_dota_hero_sven = {Spell = "sven_storm_bolt"},
+	npc_dota_hero_tiny = {Spell = "tiny_avalanche"},
+	npc_dota_hero_vengefulspirit = {Spell = "vengefulspirit_magic_missile"},
+	npc_dota_hero_riki = {Spell = "riki_smoke_screen"},
+	npc_dota_hero_enigma = {Spell = "enigma_malefice"},
+	npc_dota_hero_death_prophet = {Spell = "death_prophet_silence"},
+	npc_dota_hero_night_stalker = {Spell = "night_stalker_crippling_fear"},
+	npc_dota_hero_doom_bringer = {Spell = "doom_bringer_doom"},
+	npc_dota_hero_oracle = {Spell = "oracle_fates_edict"},
+	npc_dota_hero_zuus = {Spell = "zuus_lightning_bolt"},
+	--[[npc_dota_hero_chaos_knight = {Spell = "chaos_knight_chaos_bolt"},]]
+	--[[npc_dota_hero_ogre_magi = {Spell = "ogre_magi_fireblast"},]]
+	--[[npc_dota_hero_tusk = {Spell = "death_prophet_silence"},]]
+	--[[npc_dota_hero_batrider = {Spell = "batrider_flaming_lasso"},]]
+	--[[npc_dota_hero_skeleton_king = {Spell = "death_prophet_silence"},]]
+}
+
+Initiation = {
+	npc_dota_hero_faceless_void = {Spell = "faceless_void_time_walk"},
+	npc_dota_hero_shredder = {Spell = "shredder_timber_chain"},
+	npc_dota_hero_phoenix = {Spell = "phoenix_icarus_dive"},
+	npc_dota_hero_antimage = {Spell = "antimage_blink"},
+	npc_dota_hero_legion_commander = {Spell = "legion_commander_duel"},
+	npc_dota_hero_mirana = {Spell = "mirana_leap"},
+	npc_dota_hero_phantom_lancer = {Spell = "phantom_lancer_doppelwalk"},
+	npc_dota_hero_terrorblade = {Spell = "terrorblade_sunder"},
+	npc_dota_hero_huskar = {Spell = "huskar_life_break"},
+	npc_dota_hero_rattletrap = {Spell = "rattletrap_hookshot"},
+	npc_dota_hero_earth_spirit = {Spell = "earth_spirit_rolling_boulder"},
+	npc_dota_hero_chaos_knight = {Spell = "chaos_knight_reality_rift"},
+	npc_dota_hero_morphling = {Spell = "morphling_waveform"},
+	npc_dota_hero_vengefulspirit = {Spell = "vengefulspirit_nether_swap"},
+	npc_dota_hero_phantom_assassin = {Spell = "phantom_assassin_phantom_strike"},
+	npc_dota_hero_riki = {Spell = "riki_blink_strike"},
+	npc_dota_hero_weaver = {Spell = "weaver_time_lapse"},
+	npc_dota_hero_sandking = {Spell = "sandking_epicenter"},
+	npc_dota_hero_slark = {Spell = "slark_pounce"},
+	npc_dota_hero_crystal_maiden = {Spell = "crystal_maiden_freezing_field"},
+	npc_dota_hero_pudge = {Spell = "pudge_dismember"},
+	npc_dota_hero_Bane = {Spell = "bane_fiends_grip"},
+	npc_dota_hero_Enigma = {Spell = "enigma_black_hole"},
+	npc_dota_hero_witch_doctor = {Spell = "witch_doctor_death_ward"},
+	npc_dota_hero_queenofpain = {Spell = "queenofpain_blink"},
+	npc_dota_hero_storm_spirit = {Spell = "storm_spirit_ball_lightning"},
+	npc_dota_hero_puck = {Spell = "puck_illusory_orb"},
+	npc_dota_hero_magnataur = {Spell = "magnataur_skewer"},
+	npc_dota_hero_ember_spirit = {Spell = "ember_spirit_fire_remnant"},
+	--[[npc_dota_hero_furion = {Spell = "furion_teleportation"},]]
+	--[[npc_dota_hero_pugna = {Spell = "pugna_life_drain"},]]
+}
+
 function Key(msg,code)
 	if IsKeyDown(config.Active) and not client.chat then
 		active = not active
 		if active then
-			statusText.text = "Disabler: All"
+			statusText.text = "Disabler: Blink or attack order"
 		else
 			statusText.text = "Disabler: Blink"
 		end
@@ -48,38 +99,24 @@ function Tick( tick )
 	if not PlayingGame() or sleepTick and sleepTick > tick then return end
 	me = entityList:GetMyHero()
 	
-	local enemies = entityList:GetEntities({type=LuaEntity.TYPE_HERO,team=me:GetEnemyTeam(),illusion=false})
+	local enemies = entityList:GetEntities({type = LuaEntity.TYPE_HERO, alive = true, team = me:GetEnemyTeam(), illusion = false})
 	for i,v in ipairs(enemies) do
 		target = v
-		local IV  = v:IsInvul()
-		local MI  = v:IsMagicImmune()
-		local ST  = v:IsStunned()
-		local HEX = v:IsHexed()
-		local SI  = v:IsSilenced()
-		local DA  = v:IsDisarmed()
-		local invis = me:IsInvisible()
-		local chanel = me:IsChanneling()
-		local items = me:CanUseItems()
-		local blink = v:FindItem("item_blink")
-		local forcestaff = v:FindItem("item_force_staff")
+		local IV, MI, ST, HEX, SI, DA  = v:IsInvul(), v:IsMagicImmune(), v:IsStunned(), v:IsHexed(), v:IsSilenced(), v:IsDisarmed()
+		local invis, chanel, items = me:IsInvisible(), me:IsChanneling(), me:CanUseItems()
+		local blink, forcestaff = v:FindItem("item_blink"), v:FindItem("item_force_staff")
 		local dark_pact = v:FindModifier("modifier_slark_dark_pact") or v:FindModifier("modifier_slark_dark_pact_pulses")
 	
 		if me.alive and v.alive and v.visible then
 			if items and not (IV or MI or invis or chanel or dark_pact) then
 				if (blink and blink.cd > 11) or (forcestaff and forcestaff.cd > 18.6) then
-					UseMedalliontarget()
-					UseRodtarget()
-					LinkensSelf()
-				elseif active then
-					UseMedalliontarget()
-					UseRodtarget()
-					LinkensSelf()
+					UseMedalliontarget() UseRodtarget()
+				elseif active and entityList:GetMyPlayer().orderId == Player.ORDER_ATTACKENTITY and entityList:GetMyPlayer().target then
+					UseMedalliontarget() UseRodtarget()
 				elseif Initiation[v.name] then
 					local Spell = v:FindSpell(Initiation[v.name].Spell)
 					if Spell and Spell.level ~= 0 and Spell.cd > Spell:GetCooldown(Spell.level) - 1.6 then
-						UseMedalliontarget()
-						UseRodtarget()
-						LinkensSelf()
+						UseMedalliontarget() UseRodtarget()
 					end
 				end
 			end
@@ -88,49 +125,16 @@ function Tick( tick )
 		if me.alive and v.alive and v.visible and not hero[i] then
 			if items and not (IV or MI or ST or HEX or SI or DA or invis or chanel or dark_pact) then
 				if (blink and blink.cd > 11) or (forcestaff and forcestaff.cd > 18.6) then
-					UseHex()
-					UseSheepStickTarget()
-					UseImmediateStun()
-					UseAbyssaltarget()
-					UseBatriderLasso()
-					UseLegionDuel()
-					UseOrchidtarget()
-					UseSkysSeal()
-					UsePucksRift()
-					UseHeroSpell()
-					UseEulScepterTarget()
-					UseAstral()
-					UseHalberdtarget()
-					UseEtherealtarget()
-				elseif active then
-					UseHex()
-					UseSheepStickTarget()
-					UseImmediateStun()
-					UseAbyssaltarget()
-					UseBatriderLasso()
-					UseLegionDuel()
-					UseOrchidtarget()
-					UseSkysSeal()
-					UsePucksRift()
-					UseEulScepterTarget()
-					UseAstral()
-					UseHalberdtarget()
+					UseHex() UseSheepStickTarget() UseImmediateStun() UseAbyssaltarget() UseBatriderLasso() UseLegionDuel() UseOrchidtarget() UseSkysSeal()
+					UsePucksRift() UseHeroSpell() UseEulScepterTarget() UseAstral() UseHalberdtarget() UseEtherealtarget() EmberSpirit()
+				elseif active and entityList:GetMyPlayer().orderId == Player.ORDER_ATTACKENTITY and entityList:GetMyPlayer().target or (blink and blink.cd > 11) or (forcestaff and forcestaff.cd > 18.6) then
+					UseHex() UseSheepStickTarget() UseImmediateStun() UseAbyssaltarget() UseBatriderLasso() UseLegionDuel() UseOrchidtarget() UseSkysSeal()
+					UsePucksRift() UseEulScepterTarget() UseAstral() UseHalberdtarget() EmberSpirit()
 				elseif Initiation[v.name] then
 					local Spell = v:FindSpell(Initiation[v.name].Spell)
 					if Spell and Spell.level ~= 0 and Spell.cd > Spell:GetCooldown(Spell.level) - 1.6 then
-						UseHex()
-						UseSheepStickTarget()
-						UseImmediateStun()
-						UseAbyssaltarget()
-						UseBatriderLasso()
-						UseOrchidtarget()
-						UseSkysSeal()
-						UsePucksRift()
-						UseHeroSpell()
-						UseEulScepterTarget()
-						UseAstral()
-						UseHalberdtarget()
-						UseEtherealtarget()
+						UseHex() UseSheepStickTarget() UseImmediateStun() UseAbyssaltarget() UseBatriderLasso() UseOrchidtarget() UseSkysSeal() UsePucksRift()
+						UseHeroSpell() UseEulScepterTarget() UseAstral() UseHalberdtarget() UseEtherealtarget() EmberSpirit()
 					end
 				end
 			end
@@ -163,7 +167,6 @@ function UseEulScepterTarget()
 				me:SafeCastAbility(disable,target)
 				activated = 1
 				sleepTick = GetTick() + 100
-				return
 			end
 		end
 	end
@@ -177,7 +180,6 @@ function UseSheepStickTarget()
 				me:SafeCastAbility(disable,target)
 				activated = 1
 				sleepTick = GetTick() + 100
-				return
 			end
 		end
 	end
@@ -191,7 +193,6 @@ function UseOrchidtarget()
 				me:SafeCastAbility(disable,target)
 				activated = 1
 				sleepTick = GetTick() + 100
-				return
 			end
 		end
 	end
@@ -205,7 +206,6 @@ function UseAbyssaltarget()
 				me:SafeCastAbility(disable,target)
 				activated = 1 
 				sleepTick = GetTick() + 100
-				return
 			end
 		end
 	end
@@ -219,7 +219,6 @@ function UseHalberdtarget()
 				me:SafeCastAbility(disable,target)
 				activated = 1
 				sleepTick = GetTick() + 100
-				return
 			end
 		end
 	end
@@ -233,7 +232,6 @@ function UseEtherealtarget()
 				me:SafeCastAbility(disable,target)
 				activated = 1
 				sleepTick = GetTick() + 100
-				return
 			end
 		end
 	end
@@ -245,20 +243,16 @@ function UseRodtarget()
 		if target and GetDistance2D(me,target) < disable.castRange then
 			me:SafeCastAbility(disable,target)
 			sleepTick = GetTick() + 100
-			return
 		end
 	end
 end
 
 function UseMedalliontarget()
-	if me.health/me.maxHealth > 0.1 then
-		local disable = me:FindItem("item_medallion_of_courage")
-		if disable and disable:CanBeCasted() then
-			if target and GetDistance2D(me,target) < disable.castRange then
-				me:SafeCastAbility(disable,target)
-				sleepTick = GetTick() + 100
-				return
-			end
+	local disable = me:FindItem("item_medallion_of_courage") or me:FindItem("item_solar_crest")
+	if disable and disable:CanBeCasted() then
+		if target and GetDistance2D(me,target) < disable.castRange then
+			me:SafeCastAbility(disable,target)
+			sleepTick = GetTick() + 100
 		end
 	end
 end
@@ -278,7 +272,6 @@ function UseHex()
 				me:SafeCastAbility(disable,target)
 				activated = 1
 				sleepTick = GetTick() + 100
-				return
 			end
 		end
 	end
@@ -299,18 +292,8 @@ function UseAstral()
 				me:SafeCastAbility(disable,target)
 				activated = 1
 				sleepTick = GetTick() + 100
-				return
 			end
 		end
-	end
-end
-
-function LinkensSelf()
-	local disable = me:FindItem("item_sphere")
-	if disable and disable:CanBeCasted() then	
-		me:SafeCastAbility(disable,me)
-		sleepTick = GetTick() + 100
-		return
 	end
 end
 
@@ -329,7 +312,6 @@ function UseImmediateStun()
 				me:SafeCastAbility(disable,target)
 				activated = 1
 				sleepTick = GetTick() + 100
-				return
 			end
 		end
 	end
@@ -343,7 +325,6 @@ function UseBatriderLasso()
 				me:SafeCastAbility(disable,target)
 				activated = 1
 				sleepTick = GetTick() + 100
-				return
 			end
 		end
 	end
@@ -357,7 +338,6 @@ function UseLegionDuel()
 				me:SafeCastAbility(disable,target)
 				activated = 1
 				sleepTick = GetTick() + 100
-				return
 			end
 		end
 	end
@@ -371,7 +351,6 @@ function UseSkysSeal()
 				me:SafeCastAbility(disable,target)
 				activated = 1
 				sleepTick = GetTick() + 100
-				return
 			end
 		end
 	end
@@ -385,7 +364,24 @@ function UsePucksRift()
 				me:SafeCastAbility(disable)
 				activated = 1
 				sleepTick = GetTick() + 100
-				return
+			end
+		end
+	end
+end
+
+function EmberSpirit()
+	if activated == 0 then
+		local remnant = entityList:GetEntities(function (v) return v.npc and v.name == "npc_dota_ember_spirit_remnant" and v.alive end)
+		local disable, activate = me:FindSpell("ember_spirit_searing_chains"), me:FindSpell("ember_spirit_activate_fire_remnant")
+		if #remnant == 1 and activate and activate:CanBeCasted() and me:CanCast() then
+			me:SafeCastAbility(activate,me.position)
+			activated = 1
+			sleepTick = GetTick() + 1000
+		elseif #remnant == 0 and disable and disable:CanBeCasted() and me:CanCast() then
+			if target and GetDistance2D(me,target) < 400 then
+				me:SafeCastAbility(disable)
+				activated = 1
+				sleepTick = GetTick() + 100
 			end
 		end
 	end
@@ -400,7 +396,6 @@ function UseHeroSpell()
 					me:SafeCastAbility(disable,target)
 					activated = 1
 					sleepTick = GetTick() + 100
-					return
 				end
 			end
 		end
@@ -418,11 +413,9 @@ function Load()
 end
 
 function Close()
-	statusText.visible = false
-	hero = {}
-	icon = {}
-	collectgarbage("collect")
 	if play then
+		statusText.visible, hero, icon = false, {}, {}
+		collectgarbage("collect")
 		script:UnregisterEvent(Tick)
 		script:UnregisterEvent(Key)
 		script:RegisterEvent(EVENT_TICK,Load)
