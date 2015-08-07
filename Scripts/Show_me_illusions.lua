1b7ed50dfd499920d5aa147303fa126db0ea2322
AgeDK <Alex.Samsurs@gmail.com>
2015-08-08 01:31:03 +0300

    Update Show_me_illusions.lua

--- Scripts/Show_me_illusions.lua
+++ Scripts/Show_me_illusions.lua
@@ -1,60 +1,57 @@
-require('libs.DrawManager3D')
+-- Load
 
-local play = false
-local illusionTable = {}
-local heroTable = {}
+local heroTable = {} local illusionTable = {} local me = nil local loaded = false  local sleepTick = 0
 
-function Tick(tick)
-    if not SleepCheck() or not PlayingGame() then return end Sleep(200)
-	for _, heroEntity in ipairs(entityList:FindEntities({type = LuaEntity.TYPE_HERO, illusion = true, team = entityList:GetMyHero():GetEnemyTeam()})) do
+function Load()
+	if not PlayingGame() then return end
+	me = entityList:GetMyHero() if not me then return end
+	
+	if loaded == false then
+		print("Nyan Show Illusions: Loaded! ^__^")
+		loaded = true		
+		script:RegisterEvent(EVENT_TICK, MainTick)	
+	end
+end
+
+function MainTick(tick)
+	if not PlayingGame() or tick < sleepTick then return end
+	
+	local illusions = entityList:FindEntities({type=LuaEntity.TYPE_HERO,illusion=true,team = (5-me.team)})	--
+	local clear = false
+	for _, heroEntity in ipairs(illusions) do
 		if not (heroEntity.type == 9 and heroEntity.meepoIllusion == false) then
 			if heroEntity.visible and heroEntity.alive and heroEntity:GetProperty("CDOTA_BaseNPC","m_nUnitState") ~= -1031241196  then	
 				if not illusionTable[heroEntity.handle] then
-					illusionTable[heroEntity.handle] = drawMgr3D:CreateText(heroEntity, Vector(0,0,heroEntity.healthbarOffset),Vector2D(0,-9),0xFFFF00FF,"Illusion",drawMgr:CreateFont("defaultFont","Arial",16,1800))
+					illusionTable[heroEntity.handle] = {}
+					illusionTable[heroEntity.handle].effect1 = Effect(heroEntity,"shadow_amulet_active_ground_proj")			
+					illusionTable[heroEntity.handle].effect2 = Effect(heroEntity,"smoke_of_deceit_buff")
+					illusionTable[heroEntity.handle].effect3 = Effect(heroEntity,"smoke_of_deceit_buff")			
+					illusionTable[heroEntity.handle].effect4 = Effect(heroEntity,"smoke_of_deceit_buff")
 				end
 			else
 				if illusionTable[heroEntity.handle] then
-					illusionTable[heroEntity.handle]:Destroy()
 					illusionTable[heroEntity.handle] = nil
-				end
-			end
-		end
-	end	
-
-	for _, heroEntity in ipairs(entityList:FindEntities({type = LuaEntity.TYPE_HERO, illusion = false, team = entityList:GetMyHero():GetEnemyTeam()})) do
-		if heroEntity.classId == CDOTA_Unit_Hero_PhantomLancer or heroEntity.classId == CDOTA_Unit_Hero_Naga_Siren or heroEntity.classId == CDOTA_Unit_Hero_ChaosKnight then
-			if heroEntity.visible and heroEntity.alive then
-				if not heroTable[heroEntity.handle] then
-					heroTable[heroEntity.handle] = drawMgr3D:CreateText(heroEntity, Vector(0,0,heroEntity.healthbarOffset),Vector2D(0,-9),0xFF0000FF,"Real",drawMgr:CreateFont("defaultFont","Arial",16,1800))
-				end
-			else
-				if heroTable[heroEntity.handle] then
-					heroTable[heroEntity.handle]:Destroy()
-					heroTable[heroEntity.handle] = nil
+					clear = true
 				end
 			end
 		end
 	end
-end
-
-function Load()
-	if PlayingGame() then
-		play = true
-		script:RegisterEvent(EVENT_TICK,Tick)
-		script:UnregisterEvent(Load)
+	
+	if clear then
+		collectgarbage("collect")
 	end
-end
+ 
+	sleepTick = tick + 250
+end	
 
-function Close()
-	llusionTable = {}
+function GameClose()
 	heroTable = {}
+	llusionTable = {}
+	me = nil
+	loaded = false 	
+	script:UnregisterEvent(MainTick)	
 	collectgarbage("collect")
-	if play then	
-		script:UnregisterEvent(Tick)
-		script:RegisterEvent(EVENT_TICK,Load)
-		play = false
-	end
 end
  
-script:RegisterEvent(EVENT_CLOSE,Close)
+script:RegisterEvent(EVENT_CLOSE,GameClose)
 script:RegisterEvent(EVENT_TICK,Load)
